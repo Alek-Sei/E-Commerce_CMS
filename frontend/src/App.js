33f71fa726237a95b8e7d779e587d46a5927f106
Aleksei Skokov <a@1x.ee>
2020-10-11 23:33:10 +0300

    2 components created: Header & Footer Added react-bootstrap support

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import { Container } from 'react-bootstrap';
 
 const App = () => {
   return (
     <>
-      <p>Welcome to Watashi no Mise CMS</p>
+      <Header />
+      <main>
+        <Container>
+          <p>Welcome to Watashi no Mise CMS</p>
+        </Container>
+      </main>
+      <Footer />
     </>
   );
 }
